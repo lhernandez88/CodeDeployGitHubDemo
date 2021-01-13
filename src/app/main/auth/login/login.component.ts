91d6d50e472222081377b885979ec3417954d5aa
Juan Manuel Bermudez <juan.bermudez@ntsprint.com>
2021-01-13 12:46:29 -0500

    change preferred styles file format from css to scss

--- src/app/main/auth/login/login.component.ts
+++ src/app/main/auth/login/login.component.ts
@@ -5,7 +5,7 @@ import { Login } from 'src/app/model/login';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
